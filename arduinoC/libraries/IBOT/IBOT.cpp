1211f430d536c7acfabd54b842d47de48db87049
luthfie <126841107+Luthfie19@users.noreply.github.com>
2025-08-13 14:44:09 +0700

    library ibot

---
+++ arduinoC/libraries/IBOT/IBOT.cpp
@@ -0,0 +1,206 @@
+#include "IBOT.h"
+#include "arduino.h"
+
+// Constructor
+IBOT::IBOT()
+{
+}
+
+void IBOT::begin(int in1, int in2, int in3, int in4)
+{
+  _in1 = in1;
+  _in2 = in2;
+  _in3 = in3;
+  _in4 = in4;
+
+  _speedA = 200; // default speed
+  _speedB = 200;
+
+  pinMode(_in1, OUTPUT);
+  pinMode(_in2, OUTPUT);
+  pinMode(_in3, OUTPUT);
+  pinMode(_in4, OUTPUT);
+}
+
+long IBOT::readUltrasonicCM(int trigPin, int echoPin)
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, 25000); // timeout 25ms
+  long distance = duration * 0.034 / 2;
+  return distance;
+}
+
+void IBOT::setKipasPin(int kipas1, int kipas2)
+{
+  _kipas1 = kipas1;
+  _kipas2 = kipas2;
+
+  pinMode(_kipas1, OUTPUT);
+  pinMode(_kipas2, OUTPUT);
+
+  _kipasInitialized = true;
+}
+// Motor kiri (PWM A, PWM B)
+void IBOT::motorKiri(int pwmA, int pwmB)
+{
+  analogWrite(_in1, pwmA);
+  analogWrite(_in2, pwmB);
+}
+
+// Motor kanan (PWM A, PWM B)
+void IBOT::motorKanan(int pwmA, int pwmB)
+{
+  analogWrite(_in3, pwmA);
+  analogWrite(_in4, pwmB);
+}
+
+// Maju
+void IBOT::maju()
+{
+  motorKiri(_speedA, 0);
+  motorKanan(_speedB, 0);
+}
+
+// Mundur
+void IBOT::mundur()
+{
+  motorKiri(0, _speedA);
+  motorKanan(0, _speedB);
+}
+
+// Belok kiri
+void IBOT::belokKiri()
+{
+  motorKiri(0, _speedA);
+  motorKanan(_speedB, 0);
+}
+
+// Belok kanan
+void IBOT::belokKanan()
+{
+  motorKiri(_speedA, 0);
+  motorKanan(0, _speedB);
+}
+
+// Stop
+void IBOT::stop()
+{
+  motorKiri(0, 0);
+  motorKanan(0, 0);
+  delay(10);
+}
+
+// Set speed
+void IBOT::setSpeed(int speedA, int speedB)
+{
+  _speedA = constrain(speedA, 0, 255);
+  _speedB = constrain(speedB, 0, 255);
+}
+
+// lamp
+void IBOT::setLampPin(int lampPin)
+{
+  _lampPin = lampPin;
+  pinMode(_lampPin, OUTPUT);
+  _lampInitialized = true;
+}
+
+void IBOT::LampOn()
+{
+  if (_lampInitialized)
+  {
+    digitalWrite(_lampPin, HIGH);
+  }
+}
+
+void IBOT::LampOff()
+{
+  if (_lampInitialized)
+  {
+    digitalWrite(_lampPin, LOW);
+  }
+}
+
+// HORN
+void IBOT::setHornPin(int hornPin)
+{
+  _hornPin = hornPin;
+  pinMode(_hornPin, OUTPUT);
+  _hornInitialized = true;
+}
+
+void IBOT::HornOn()
+{
+  if (_hornInitialized)
+  {
+    digitalWrite(_hornPin, HIGH);
+  }
+}
+
+void IBOT::HornOff()
+{
+  if (_hornInitialized)
+  {
+    digitalWrite(_hornPin, LOW);
+  }
+}
+
+// Kipas
+void IBOT::kipasOn()
+{
+  if (_kipasInitialized)
+  {
+    analogWrite(_kipas1, 0);
+    analogWrite(_kipas2, 200);
+  }
+}
+
+void IBOT::kipasOff()
+{
+  if (_kipasInitialized)
+  {
+    analogWrite(_kipas1, 0);
+    analogWrite(_kipas2, 0);
+  }
+}
+// Flame
+void IBOT::setFlamePin(int analogPin, int digitalPin)
+{
+  _flameAnalogPin = analogPin;
+  _flameDigitalPin = digitalPin;
+  pinMode(_flameAnalogPin, INPUT);
+  pinMode(_flameDigitalPin, INPUT);
+  _flameInitialized = true;
+}
+
+int IBOT::readFlameAnalog()
+{
+  if (_flameInitialized)
+  {
+    return analogRead(_flameAnalogPin);
+  }
+  else
+  {
+    return -1; // belum di-set
+  }
+}
+
+bool IBOT::readFlameDigital()
+{
+  if (_flameInitialized)
+  {
+    return digitalRead(_flameDigitalPin) == LOW; // LOW = api terdeteksi
+  }
+  else
+  {
+    return false;
+  }
+}
---
+++ arduinoC/libraries/IBOT/IBOT.h
@@ -0,0 +1,52 @@
+#ifndef IBOT_H
+#define IBOT_H
+
+class IBOT {
+  public:
+    // Constructor
+    IBOT();
+    void begin(int in1, int in2, int in3, int in4);
+	void setKipasPin(int kipas1, int kipas2); // assign kipas pin di setup
+    void setLampPin (int lampPin);
+    void setHornPin (int hornPin);
+
+    // Method kontrol gerakan
+    void maju();
+    void mundur();
+    void belokKiri();
+    void belokKanan();
+    void stop();
+    void LampOn();
+    void LampOff();
+    void HornOn();
+    void HornOff();
+	void kipasOn();
+    void kipasOff();
+    long readUltrasonicCM(int trigPin, int echoPin);
+    void setFlamePin(int analogPin, int digitalPin);
+    int  readFlameAnalog();
+    bool readFlameDigital();
+
+    // Set speed
+    void setSpeed(int speedA, int speedB);
+
+  private:
+    int _in1, _in2, _in3, _in4;
+	int _kipas1,_kipas2;
+    int _lampPin;
+    int _hornPin;
+    int _speedA,_speedB;
+    int _trigPin, _echoPin;
+    int _flameAnalogPin;
+    int _flameDigitalPin;
+    bool _flameInitialized = false;
+    bool _ultrasonicInitialized = false;
+    bool _hornInitialized = false;            // <- Tambahan
+    bool _lampInitialized = false;            // <- Tambahan
+	bool _kipasInitialized = false; // biar aman kalau kipas belum di-setup
+
+    void motorKiri(int pwmA, int pwmB);
+    void motorKanan(int pwmA, int pwmB);
+};
+
+#endif
